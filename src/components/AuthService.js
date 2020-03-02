cff2a54fc0ae8a3ee9ddf1ffc04e8e8e4922a244
David <davidyuen0305@gmail.com>
2020-03-02 15:28:49 -0800

    added a comment, login and sign up not working on deployed app. Will fix the bug .

--- src/components/AuthService.js
+++ src/components/AuthService.js
@@ -1,3 +1,4 @@
+//might have to change the fetch path to heroku in order for deployed app to work
 export const _signUp = (username, password, phonenumber) => {
 	return fetch("http://localhost:5000/signup/", {
 		method: 'POST',
@@ -8,7 +9,7 @@ export const _signUp = (username, password, phonenumber) => {
 		body: JSON.stringify({ username, password, phonenumber })
 	}).then(res => res.json())
 }
-
+//might have to change the fetch path to heroku in order for deployed app to work
 export const _login = (username, password) => {
 	return fetch("http://localhost:5000/login/", {
 		method: 'POST',
